46195b0c485c31b9b5a1f9fbfb1f9f9e25ed6c59
27jpl1 <japylyon@gmail.com>
2025-09-30 12:28:52 -0500

    TF-IDF Code Done

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,4 +1,7 @@
-# A method that return the list of terms and a histogram of how many documents each term appears in
+# A method that returns the list of terms for all documents and a histogram of how many documents each term appears in
+import math
+
+
 def filter_terms(document_dictionary):
     term_list = []          # A list of all terms found in the corpus (This may need to be a set then convert to list)
     term_hist = {}          # A histogram of the number of documents the term appears in
@@ -15,26 +18,45 @@ def filter_terms(document_dictionary):
                     term_hist[term] += 1
     return term_list, term_hist
 
+# A function to create a histogram of the number of times each term in a document appears
+def document_term_frequency(doc_contents: list):
+    doc_hist = {}
+    for term in doc_contents:
+        if term not in doc_hist.keys():
+            doc_hist[term] = 1
+        else:
+            doc_hist[term] += 1
+    return doc_hist
+
 
 def tfidf(corpus):
     doc_dict = {}           # A dictionary where keys are document names and values are a list of words of what the document says
-    doc_list = []           # A list of the names of the documents (Can just get this from doc_dict keys I think)
+    doc_scores = {}         # A dictionary where keys a document names and values are idf scores for that document
     for line in corpus:     # For each line(document) in the corpus, add it to doc_dict and doc_list
         line = line.strip()
         doc, words = line.split("|")          # The document name comes before the | and the words come after
         doc_dict[doc] = words.split()         # Adds the document to the dictionary with the words split into a list
-        doc_list.append(doc)                  # Adds the name of the document to the document list
     term_list, term_hist = filter_terms(doc_dict)
+    for doc in doc_dict.keys():
+        doc_term_hist = document_term_frequency(doc_dict[doc])      # get the frequency of terms in a document
+        idf_scores = []                       # A list of idf score for each term
+        for term in term_list:                # Append the score of each term to the list
+            if term in doc_term_hist.keys():
+                score = (doc_term_hist[term]/len(doc_dict[doc])) * math.log(len(doc_dict.keys())/term_hist[term])
+                idf_scores.append(score)
+            else:
+                idf_scores.append(0)
+        doc_scores[doc] = idf_scores
 
+    # could have a function create a list of term_idf_scores
+    # then just loop through documents and find term frequency for every term and multiply to get score
+    # keep scores in list and then have it as a value to a document key
     print(doc_dict)
     print(term_list)
     print(term_hist)
-    # I now have term list and number of documents each term is found in
-    # Need to determine idf score and tf score next
-    # Eventually return a dictionary with keys as documents and values as idf scores
-    # TODO Remember that the list of idf scores need to include every term (so any term not found in a document has a score of 0)
-
-    return 0
+    for doc in doc_dict:
+        print(doc, doc_scores[doc])
+    return doc_scores
 
 
 document = open("Who Wrote the Bible Verse Combinations", "r")
